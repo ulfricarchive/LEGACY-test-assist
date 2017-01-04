23f2c770555791d1e1fda4c60ff42f41b4f7a812
Adam Harrison <ad@mharrison.us>
2017-01-03 22:10:15 -0500

    Upload util test base

---
+++ src/main/java/com/ulfric/testing/UtilTestBase.java
@@ -0,0 +1,23 @@
+package com.ulfric.testing;
+
+import org.junit.jupiter.api.Test;
+
+import com.ulfric.verify.Verify;
+
+public class UtilTestBase {
+
+	private final Class<?> util = this.getClass().getAnnotation(Util.class).value();
+
+	@Test
+	void testIsEnum()
+	{
+		Verify.that(this.util).isEnum();
+	}
+
+	@Test
+	void testEnumHasNoConstants()
+	{
+		Verify.that(this.util.getEnumConstants()).isEmpty();
+	}
+
+}
