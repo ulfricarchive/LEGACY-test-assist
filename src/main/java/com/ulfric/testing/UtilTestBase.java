46cd7163e4bca8b8c4d90601e30188adac02aeba
insou22 <zac.kologlu@gmail.com>
2017-01-06 18:17:21 +1100

    Make utility enums get that sweet 100% coverage 👌🏼🎉

--- src/main/java/com/ulfric/testing/UtilTestBase.java
+++ src/main/java/com/ulfric/testing/UtilTestBase.java
@@ -1,12 +1,20 @@
 package com.ulfric.testing;
 
-import org.junit.jupiter.api.Test;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.ulfric.testing.internal.EnumBuster;
 import com.ulfric.verify.Verify;
 
+@RunWith(JUnitPlatform.class)
+@SuppressWarnings("unchecked")
 public class UtilTestBase {
 
-	private final Class<? extends Enum<?>> util = this.getClass().getAnnotation(Util.class).value();
+	private final Class<?> util = this.getClass().getAnnotation(Util.class).value();
 
 	@Test
 	void testIsEnum()
@@ -15,9 +23,29 @@ public class UtilTestBase {
 	}
 
 	@Test
-	void testEnumHasNoConstants()
+	public void test_instanceUtils_enum()
 	{
-		Verify.that(this.util.getEnumConstants()).isEmpty();
+		EnumBuster buster = new EnumBuster(this.util);
+
+		Object VALUE = buster.make("VALUE");
+		buster.addByValue((Enum) VALUE);
+
+		try
+		{
+			Method values = this.util.getDeclaredMethod("values");
+
+			values.invoke(null);
+
+			Method valueof = this.util.getDeclaredMethod("valueOf", String.class);
+
+			valueof.invoke(null, "VALUE");
+		}
+		catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}
+
+		buster.restore();
 	}
 
 }
